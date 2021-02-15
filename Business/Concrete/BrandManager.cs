dff002a6b90178f5a39b63357e794cb1e6e661df
icadir <ismailcadir@gmail.com>
2021-02-15 23:26:26 +0300

    Business Servis Ok

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class BrandManager
+    {
+        private IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+    }
+}
---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        private IColorDal _colorDal;
+
+        public ColorManager(IColorDal colorDal)
+        {
+            _colorDal = colorDal;
+        }
+    }
+}
